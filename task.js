d1a3b83050f38ab735fb90466e297c618657a6b7
Ridwan Adebayo Surajudeen <75109262+mr-ridhor@users.noreply.github.com>
2021-11-24 16:02:32 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,49 @@
+//QUESTION ONE
+// SETP 1. Start
+
+const { Console } = require("console")
+
+// STEP 2. Declare variables num1, num2 and sum
+let a  , b, c
+
+//STEP 3: Read values a and b.
+a='2', b='3' 
+// STEP 4. Add a and b and assign the result to sum
+c= +a +  +b
+// STEP 5: Display sum
+console.log(c)
+
+
+//QUESTION TWO
+  console.log("ROUND 5")
+
+
+
+
+  //QUESTION 3
+  // let text= "Sand box"
+  // let result  =text.includes("box")
+ 
+const str = ()=>{
+    let text= "Sand box";
+    result =text.includes("box")
+    let res= text.split('').sort().join('')
+if( result)
+ {console.log(`${result} ${" It is  a character in the original string"}`)
+}else{
+console.log("It is not")
+};
+if(lex=text > res) {
+ 
+  console.log( lex + " " + "Lexicologically bigger")
+}else{
+  console.log("Its not")
+}
+ 
+ if(meet= lex || result  ){
+  console.log("Conditions satisfied" )
+ }else{
+   console.log("Conditions not satisfied")
+ }
+}
+  str()
